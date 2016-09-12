464da905d585a5f853c65ab2e2f2d1313f39642f
Robert Yang <yang.robert.w@gmail.com>
2016-09-12 12:35:42 -0400

    week02: intro to code, intro to maya, simple rotation + keyboard input code (RotateCube.cs)

---
+++ week02/Assets/scripts/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// if the user holds down LeftArrow then do this...
+		if ( Input.GetKey(KeyCode.LeftArrow) ) {
+			transform.Rotate(0f, 5f, 0f); // rotate 5 degrees on X axis
+		}
+
+		// if the user holds down RightArrow then rotate 5 degrees other way
+		if ( Input.GetKey(KeyCode.RightArrow) ) {
+			transform.Rotate(0f, -5f, 0f);
+		}
+	}
+}
