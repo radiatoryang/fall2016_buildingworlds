0854bf5d2728845b5da04b06035a895989d5cdb3
Robert Yang <yang.robert.w@gmail.com>
2016-09-16 21:45:01 -0400

    week03: RandomVector.cs and MouseVector.cs

---
+++ week03/Assets/scripts/MouseVector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseVector : MonoBehaviour {
+
+	public float mouseX, mouseY;
+	
+	// Update is called once per frame
+	void Update () { 
+		// "mouse deltas"
+		// if we don't move the mouse, it's equal to 0
+		mouseX = Input.GetAxis( "Mouse X" );
+		mouseY = Input.GetAxis( "Mouse Y" );
+
+		// move object based on current mouseSpeed
+		// this will be 0,0,0 if the mouse stays still
+		// transform.position = new Vector3( mouseX, 0f, mouseY );
+
+		// move object, but ON TOP of previous position
+		transform.position += new Vector3( mouseX, 0f, mouseY );
+	}
+}
---
+++ week03/Assets/scripts/RandomVector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomVector : MonoBehaviour {
+
+	public float speed = 5f; // when you put "public" in front of a var, it shows up in Unity
+
+	float lastTimeWhenIMoved = 0f;
+	Vector3 myDestination; // remember where I want to move
+	
+	// Update is called once per frame
+	void Update () {
+		// transform.position = new Vector3( 1f, 1f, 4.20f );
+
+		// set a new destination
+		if ( Time.time > lastTimeWhenIMoved + 1f ) { // wait 1 second
+			myDestination = new Vector3( Random.Range( -2f, 2f), 
+										 Random.Range(-2f, 2f), 
+										 Random.Range(-2f, 4.20f) 
+									   );
+			lastTimeWhenIMoved = Time.time; // update last time when I moved
+		}
+
+		// ok let's actually move now
+		transform.position = Vector3.MoveTowards( transform.position,
+												  myDestination,
+												  Time.deltaTime * speed
+												);
+
+	} // end of Update()
+}
