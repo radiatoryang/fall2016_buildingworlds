23b2c68b09412d98466f136e4d1d040ce3c937c4
Robert Yang <yang.robert.w@gmail.com>
2016-09-30 18:38:06 -0400

    week05: DroneController.cs (using AddForce to move stuff around)

---
+++ week05/Assets/scripts/DroneController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// COMPUTER SCIENCE TERM TO GOOGLE IF YOU WANT TO KNOW MORE: "Model View Controller" or MVC
+
+public class DroneController : MonoBehaviour {
+
+	public GameObject droneObject;
+	public GameObject capsuleObject;
+
+
+	// FixedUpdate is where PHYSICS happens
+	void FixedUpdate () {
+		// hold down SPACE to add force
+		if ( Input.GetKey( KeyCode.Space ) ) {
+			droneObject.GetComponent<Rigidbody>()
+				.AddForce( droneObject.transform.forward * 10f );
+		}
+
+		if ( Input.GetKey(KeyCode.LeftArrow ) ) {
+			// GetComponent<Transform>()... is like saying "transform..."
+			droneObject.GetComponent<Transform>().Rotate( 0f, -5f, 0f );
+		}
+		if ( Input.GetKey(KeyCode.RightArrow ) ) {
+			droneObject.transform.Rotate( 0f, 5f, 0f );
+		}
+	}
+}
