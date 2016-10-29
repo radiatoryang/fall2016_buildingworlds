da39829d04562d36d2e144ec002279b81beb5cab
Robert Yang <yang.robert.w@gmail.com>
2016-10-29 18:11:14 -0400

    week08 / week09: intro to instantiation... TenPrintDemo.cs (random instantiate example)

---
+++ week08/Assets/SimpleRestart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+public class SimpleRestart : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// Unity 5.3 or earlier
+//		if ( Input.GetKeyDown(KeyCode.R) ) {
+//			Application.LoadLevel( Application.loadedLevel );
+//		}
+
+		// Unity 5.4 or later, "more correct" way
+		if ( Input.GetKeyDown(KeyCode.R) ) {
+			SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
+		}
+	}
+}
---
+++ week08/Assets/TenPrintDemo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TenPrintDemo : MonoBehaviour {
+	// variables to connect prefabs in the code
+	public GameObject prefabA;
+	public GameObject prefabB;
+
+	int counter = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if ( counter < 30 ) {
+			float randomNumber = Random.Range(0f, 100f);
+			if ( randomNumber < 50f ) {
+				// Transform newClone = (Transform)
+				GameObject newClone = (GameObject)Instantiate( prefabA, 
+					new Vector3( Random.Range(-5f, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f) ),
+					Quaternion.Euler( Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f) )
+				);
+				newClone.transform.localScale *= Random.Range(0f, 5f);
+			} else {
+				GameObject newClone2 = (GameObject)Instantiate( prefabB, 
+					new Vector3( Random.Range(-5f, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f) ),
+					Quaternion.Euler( Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f) )
+				);
+				newClone2.GetComponent<Renderer>().material.color = Random.ColorHSV();
+			}
+			counter++;
+		}
+
+
+	}
+}
