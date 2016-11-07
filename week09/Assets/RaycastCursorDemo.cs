2fb4f4f557ae92cab0857e5f72a976f723067cf5
Robert Yang <yang.robert.w@gmail.com>
2016-11-07 01:22:36 -0500

    week09: raycasting (search for RaycastCursorDemo.cs)

---
+++ week09/Assets/RaycastCursorDemo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastCursorDemo : MonoBehaviour {
+
+	public GameObject mySphere; // assign reference in the Inspector
+	
+	// Update is called once per frame
+	void Update () {
+		// STEP 1: define "Ray" based on mousePosition in camera view
+		Ray myRay = Camera.main.ScreenPointToRay( Input.mousePosition );
+
+		// STEP 2: save some memory for "RaycastHit"
+		RaycastHit myRayHit = new RaycastHit();
+
+		// STEP 2.5: visualize the ray in Scene View
+		Debug.DrawRay( myRay.origin, myRay.direction * 100f, Color.yellow);
+
+		// STEP 3: shoot the raycast now
+		if ( Physics.Raycast( myRay, out myRayHit, 1000f ) ) {
+			Debug.Log("I'm hitting something!");
+			// "RaycastHit.point" = Vector3 in world where the raycast hit
+			mySphere.transform.position = myRayHit.point; 
+
+			// INSTANTIATE A SPHERE WHEN WE HOLD MOUSE BUTTON
+			if ( Input.GetMouseButton(0) ) {
+				Instantiate( mySphere, myRayHit.point, Quaternion.Euler(0f, 0f, 0f) );
+			}
+
+		} else {
+			Debug.Log("I'm hitting nothing :( :( ");
+		}
+
+	}
+}
